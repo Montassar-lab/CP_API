9407f9aa0af9b4ec582a50c487e63a254d129655
Montassar-lab <montassar.abbes@gmail.com>
2023-01-20 10:28:16 +0100

    first commit

---
+++ hello-world.js
@@ -0,0 +1,31 @@
+// TASK1
+// console.log('HELLO WORLD')
+
+
+
+// TASK2
+// const http = require('http')
+
+// http.createServer((req,res)=>{
+//     res.write('<h1>Hello Node!!!!</h1>\n')
+//     res.end()
+
+// }).listen(3000)
+
+
+
+// TASK3
+// const fs = require('fs')
+
+// const bo = fs.readFileSync('./welcome.txt','utf-8')
+
+// const http = require('http')
+
+// http.createServer((req,res)=>{
+//     res.write(bo)
+//     res.end()
+// }).listen(5000, console.log('Server is running'))
+
+
+
+// TASK4
---
+++ passwordgenerator.js
@@ -0,0 +1,10 @@
+// TASK4
+var generator = require('generate-password');
+
+var password = generator.generate({
+	length: 10,
+	numbers: true
+});
+
+// 'uEyMTw32v9'
+console.log(password);
